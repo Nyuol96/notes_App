ce5414085358a25b4a63c3da77366ba2f5e889d0
Nyuol96 <121433290+Nyuol96@users.noreply.github.com>
2023-10-05 17:15:14 +0300

    create custom fields

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/views/widgets/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: TextStyle(color: kPrimaryColor),
+        border: buildborder(),
+        enabledBorder: buildborder(),
+        focusedBorder: buildborder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildborder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: color ?? Colors.white));
+  }
+}
