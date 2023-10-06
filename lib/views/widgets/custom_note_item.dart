fb716be92d49fe97c04f6a6a91873cd1f7d7affa
Nyuol96 <121433290+Nyuol96@users.noreply.github.com>
2023-10-06 15:20:16 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,49 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/widgets/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(12)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Text(
-              'Bild your career with NyUol',
-              style:
-                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 20),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Text(
+                'Bild your career with NyUol',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.4), fontSize: 20),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(
-              'October, o4, 2023',
-              style: TextStyle(color: Colors.black.withOpacity(.4)),
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text(
+                'October, o4, 2023',
+                style: TextStyle(color: Colors.black.withOpacity(.4)),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
