d57e6f0ad98b0f7c8e11a4d36a4f9b07ee7b9fb0
Nyuol96 <121433290+Nyuol96@users.noreply.github.com>
2023-11-06 16:07:19 +0200

    create cubit

--- lib/views/widgets/custom_App_Bar.dart
+++ lib/views/widgets/custom_App_Bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/views/widgets/search_icon.dart';
-
 class CostumeAppBar extends StatelessWidget {
   const CostumeAppBar({super.key, required this.title, required this.icon});
   final String title;
